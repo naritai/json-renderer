14b5248fac7eba1d6d15f8da9739c0457e52d23f
naritai <alexandr.voronetskiy@gmail.com>
2024-06-13 16:09:13 +0300

    implement virtuaisation with react-window

--- src/entities/json-member/ui/json-member-list/JSONMemberList.tsx
+++ src/entities/json-member/ui/json-member-list/JSONMemberList.tsx
@@ -1,6 +1,7 @@
 import { JSONMemberRow } from '../json-member-row/JSONMemberRow';
 import { useJsonData } from '../../model/json-member.store';
 import styles from './JSONMemberList.module.scss';
+import { FixedSizeList as List } from 'react-window';
 import cn from 'classnames';
 
 export const JSONMemberList = () => {
@@ -18,11 +19,25 @@ export const JSONMemberList = () => {
     );
   }
 
+  const Row = ({ index, key, style }: any) => {
+    const { id } = jsonData[index];
+    return (
+      <div>
+        <div key={key} style={style} className="post">
+          <JSONMemberRow key={String(id)} id={String(id)} />
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className={styles.container}>
-      {jsonData.map((el) => (
+      {/* {jsonData.map((el) => (
         <JSONMemberRow key={String(el.id)} id={String(el.id)} />
-      ))}
+      ))} */}
+      <List width={1200} height={580} itemCount={jsonData.length} itemSize={50}>
+        {Row}
+      </List>
     </div>
   );
 };
